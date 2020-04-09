0258b292ebdb65ad3a463de08f9333b3fb78fdfc
Shripad Kale <50066426+kaleshripad11@users.noreply.github.com>
2020-04-09 21:02:11 +0530

    Add files via upload

---
+++ DAY1.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print('Hello There...')
+print('My name is SHRIPAD KALE')
+
+
+# In[3]:
+
+
+# Comments  in python
+print("Is the above comment visible?")
+
+
+# In[4]:
+
+
+print("Comments are just used for refernce purpose.")
+print("Comments are never executed")
+
+
+# # What are Datatypes and Objects
+
+# In[5]:
+
+
+print("Datatypes are nothing but they are just a container which holds the data.")
+
+
+# In[6]:
+
+
+x=20
+y=20.0
+z="20.0"
+print("X is an integer var ",x)
+print("Y is an float var ",y)
+print("Z is an string var ",z)
+
+
+# In[8]:
+
+
+print("Integer(int) is an Datatype which holds numerical values like 1,10....n")
+print("Float(float) is the datatype which holds decimal values like 1.0, 200.0...n")
+print("String(str) datatype holds strings enclosed in double quotes")
+print("List is an object in python which holds different types of values")
+print("List is an ordered sequence of different type values")
+print("List is an simple series of words or numerals")
+
+
+# In[12]:
+
+
+x= 200
+y= 500
+z=x+y
+print("Integer sum : ",z)
+
+
+# In[11]:
+
+
+a=40.0
+b=45.0
+c=a+b
+print("Float sum : ",c)
+
+
+# In[13]:
+
+
+strinz="Hi there..."
+print(strinz)
+
+
+# In[14]:
+
+
+#Check the type of data
+type(strinz)
+
+
+# In[18]:
+
+
+type(c)
+
+
+# In[19]:
+
+
+type(z)
+
+
+#  # Advanced Datatypes
+
+# In[20]:
+
+
+listx=["hello",45,45.0]
+print(listx)
+
+listx[2]
+
+
+# In[25]:
+
+
+demolist=[[1,4,7],[32,56],[13,0,90,99]]
+demolist[1][1]
+
+
+# In[43]:
+
+
+listx.append(2)
+print(listx)
+
+
+# In[57]:
+
+
+listx.insert(1,7)
+print(listx)
+
+
+# In[62]:
+
+
+listx.count(7)
+#print(listx)
+
+
+# In[61]:
+
+
+listx.copy()
+print(listx)
+
+
+# In[63]:
+
+
+listx.pop(3)
+
+
+# In[66]:
+
+
+listx.sort()
+print(listx)
+
+
+# In[67]:
+
+
+listx.reverse()
+print(listx)
+
+
+# In[70]:
+
+
+listx.remove(7)
+print(listx)
+
+
+# In[75]:
+
+
+listx.index(5)
+#print(listx)
+
+
+# In[ ]:
+
+
+
+
